e4245d3ad284cd463512ffa070487f7886255ca1
Kevin MoRo <125381456+Kevin-MR-xd@users.noreply.github.com>
2023-03-09 21:55:05 -0600

    Add files via upload

--- src/imprimeprecioc/ImprimePrecioC.java
+++ src/imprimeprecioc/ImprimePrecioC.java
@@ -3,25 +3,28 @@ import java.util.Scanner;
 
 /**
  *
- * @author maryse
+ * @author El Chistes (Kevin MR)
  *
  * Scanner 
  * Este programa calcula e imprime la cantidad de una orden de compra.
  *
  */
 public class ImprimePrecioC {
-
+    Scanner stdIn = new Scanner(System.in);
+    static double precio = 0; // precio de compra del artículo
+    static double cant = 0; // número de artículos comprados
+    public void IMP(double precio, double cant){
+         System.out.print("Precio de compra del artículo: ");
+         precio = stdIn.nextDouble();
+         System.out.print("Cantidad: ");
+         cant = stdIn.nextDouble();
+         System.out.println("Total de orden de compra = $" + precio * cant);
+         
+    }
+    
     public static void main(String[] args) {
-
-        Scanner stdIn = new Scanner(System.in);
-        double precio; // precio de compra del artículo
-        int cant; // número de artículos comprados
-        
-        System.out.print("Precio de compra del artículo: ");
-        precio = stdIn.nextDouble();
-        System.out.print("Cantidad: ");
-        cant = stdIn.nextInt();
-        System.out.println("Total de orden de compra = $" + precio * cant);
+      ImprimePrecioC IMP = new ImprimePrecioC();
+      IMP. IMP(precio,cant);
     }
 
 }
